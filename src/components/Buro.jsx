4e310392b1a23c7f417922beb28c8a66bf80e8dc
alexeerma <aleksander.eerma@hotmail.com>
2023-03-25 13:19:46 +0200

    Layout of Fields component, need styling

--- src/components/Buro.jsx
+++ src/components/Buro.jsx
@@ -5,9 +5,9 @@ const Buro = () => (
   
   <section>
     <div>
-      <h1 className='text-center text-black font-displayfair text-6xl py-[30px]'>Büroo</h1>
-      <div className='flex py-[60px]'>
-        <div className='w-1/2'>
+      <h1 className='text-center text-primary font-displayfair font-semibold text-6xl pt-[100px] pb-[60px]'>Büroo</h1>
+      <div className='flex text-justify justify-between py-[60px] mx-[120px]'>
+        <div className='w-[550px]'>
           <p>Confere Advokaadibüroo OÜ asutajaks on vandeadvokaat Kersti Kägi, kes advokaadina on tegutsenud üle 25 aasta. Meie tugevuseks on pikaajaline õigusnõustamise ja kohtumenetlustes esindamise kogemus enamikes õigusvaldkondades. Meie hindame professionaalsust, põhjalikkust, kvaliteeti, ratsionaalsust ja juhindume oma töös nendest väärtustest. Vajadusel teeme koostööd erinevate teiste elualade spetsialistidega ja kolleegidega teistest advokaadibüroodest.</p>
         </div>
         <div>
--- src/components/Fields.jsx
+++ src/components/Fields.jsx
@@ -5,79 +5,79 @@ const Fields = () => (
 
   <section>
     <div>
-      <h1>Valdkonnad</h1>
+      <h1 className="text-center text-primary text-6xl font-semibold font-displayfair pt-[50px]">Valdkonnad</h1>
     </div>
-    <div>
+    <div className="flex justify-between pt-[100px] mx-[130px] pb-[50px]">
       <div>
-        <h2>Asjaõigus</h2>
-        <p>Asjaõigused on omand omandiõigus ja piiratud asjaõigused: servituudid, reaalkoormatised, hoonestusõigus, ostueesõigus ja pandiõigus. Nõustame erinevates asjaõiguslikes küsimustes, aitame koostada vajalikke õigusdokumente ja esindame kinnisasjadega seotud vaidlustes.</p>
+        <h2 className="text-black font-nunito text-3xl font-md pb-[15px]">Asjaõigus</h2>
+        <p className="box-border text-justify w-[800px]">Asjaõigused on omand omandiõigus ja piiratud asjaõigused: servituudid, reaalkoormatised, hoonestusõigus, ostueesõigus ja pandiõigus. Nõustame erinevates asjaõiguslikes küsimustes, aitame koostada vajalikke õigusdokumente ja esindame kinnisasjadega seotud vaidlustes.</p>
       </div>
-      <img src={asjaoigus} alt="" />
+      <img src={asjaoigus} alt="" className="w-[200px] h-[200px] max-w-[208px]" />
     </div>
-    <div>
+    <div className="flex flex-row-reverse justify-between py-[30px] mx-[130px]">
       <div>
-        <h2>Maksud ja maksuvaidlused</h2>
-        <p>Maksustamist reguleerivate õigusaktide tundmine ja praktilised oskused maksumenetlustest võimaldavad meil hinnata, millised teemad maksuhaldurit kõige enam huvitavad. Nõustame ja esindame suhtlemisel maksuhalduriga. Aitame koostada vaide või kaebuse maksuhalduri korraldusele, maksuotsusele ja teistele dokumentidele ning esindame maksumenetluses ja halduskohtus.</p>
+        <h2 className="text-black text-right font-nunito text-3xl font-md pb-[15px]">Maksud ja maksuvaidlused</h2>
+        <p className="box-border w-[800px] text-justify">Maksustamist reguleerivate õigusaktide tundmine ja praktilised oskused maksumenetlustest võimaldavad meil hinnata, millised teemad maksuhaldurit kõige enam huvitavad. Nõustame ja esindame suhtlemisel maksuhalduriga. Aitame koostada vaide või kaebuse maksuhalduri korraldusele, maksuotsusele ja teistele dokumentidele ning esindame maksumenetluses ja halduskohtus.</p>
       </div>
-      <img src={maksuoigus} alt="" />
+      <img src={maksuoigus} alt="" className="w-[200px] h-[200px] max-w-[208px]" />
     </div>
-    <div>
+    <div className="flex justify-between pt-[100px] mx-[130px] pb-[50px]">
       <div>
-        <h2>Maksejõuetus. Pankrot ja saneerimine</h2>
-        <p>Pankrot on võlgniku kohtuotsusega väljakuulutatud maksejõuetus. Võlgnik on maksejõuetu, kui ta ei suuda rahuldada võlausaldaja nõudeid ja see suutmatus ei ole võlgniku majanduslikust olukorrast tulenevalt ajutine. Aitame koostada pankrotihoiatust ja pankrotiavaldust kohtule, nõustame ning esindame ettevõtteid ja eraisikuid pankrotimenetlustes ja kohtus. Saneerimine on abinõude rakendamine eraõigusliku juriidilise isiku suhtes ettevõtja majanduslike raskuste ületamiseks, tema likviidsuse taastamiseks, kasumlikkuse parandamiseks ja jätkusuutliku majandamise tagamiseks.</p>
+        <h2 className="text-black font-nunito text-3xl font-md pb-[15px]">Maksejõuetus. Pankrot ja saneerimine</h2>
+        <p className="box-border text-justify w-[800px]">Pankrot on võlgniku kohtuotsusega väljakuulutatud maksejõuetus. Võlgnik on maksejõuetu, kui ta ei suuda rahuldada võlausaldaja nõudeid ja see suutmatus ei ole võlgniku majanduslikust olukorrast tulenevalt ajutine. Aitame koostada pankrotihoiatust ja pankrotiavaldust kohtule, nõustame ning esindame ettevõtteid ja eraisikuid pankrotimenetlustes ja kohtus. Saneerimine on abinõude rakendamine eraõigusliku juriidilise isiku suhtes ettevõtja majanduslike raskuste ületamiseks, tema likviidsuse taastamiseks, kasumlikkuse parandamiseks ja jätkusuutliku majandamise tagamiseks.</p>
       </div>
-      <img src={piggy} alt="" />
+      <img src={piggy} alt="" className="w-[200px] h-[200px] max-w-[208px]" />
     </div>
-    <div>
+    <div className="flex flex-row-reverse justify-between py-[30px] mx-[130px]">
       <div>
         <h2>Meditsiiniõigus. Tervishoiuteenused</h2>
         <p>Meditsiiniõigus reguleerib tervishoiuteenuse osutaja ning patsientide õigusi ja kohustusi. Meie advokaadibüroo on nõustanud ja esindanud tervishoiuteenuse osutamisest tõusetunud vaidluste lahendamisel nii tervishoiuteenuse osutaja kui ka patsiendi poolel.</p>
       </div>
       <img src={meditsiin} alt="" />
     </div>
-    <div>
+    <div className="flex justify-between pt-[100px] mx-[130px] pb-[50px]">
       <div>
         <h2>Perekonnaõigus</h2>
         <p>Perekonnaõigus reguleerib abikaasade omavahelisi suhteid, vanemate ja laste vahelisi suhteid, vanemate kohustusi laste ees, elatist jms. Sellepärast on perekonnaõigus delikaatseimaid ja emotsionaalselt keerukaimaid õigusvaldkondi. Meie advokaadibüroo nõustab ja esindab kliente kõikides perekonnaõiguslikes küsimustes, aitab koostada vajalikke õigusdokumente ning esindab kliente läbirääkimistel suhetes kolmandate isikutega ja kohtus.</p>
       </div>
       <img src={perekonnaoigus} alt="" />
     </div>
-    <div>
+    <div className="flex flex-row-reverse justify-between py-[30px] mx-[130px]">
       <div>
         <h2>Pärimisõigus</h2>
         <p>Pärimine on isiku surma korral tema vara üleminek teisele isikule. Testamendi ja pärimislepingu ning teised pärimisega seotud toiminuid tõestab notar, kuid me saame nõustada pärimistoimingute tegemisel. Nõustame ka pärandi vastuvõtmisel, pärandaja kohustuste kindlakstegemisel ja pärandi jagamisel ning aitame leida notari pärimistoimingute tegemiseks.</p>
       </div>
       <img src={parimine} alt="" />
     </div>
-    <div>
+    <div className="flex justify-between pt-[100px] mx-[130px] pb-[50px]">
       <div>
         <h2>Täitemenetlus</h2>
         <p>Täitedokumendid on nimetatud täitemenetluse seadustikus. Üldtuntumad on erinevad riigi- ja omavalitsusasutuste määratud trahviotsused, kohtute otsused ja määrused ning muud täitedokumendid. Aitame koostada täiteavaldust, nõustame ja esindame kõikides täitemenetlusega kaasnevate küsimuste lahendamisel.</p>
       </div>
       <img src={taitemenetlus} alt="" />
     </div>
-    <div>
+    <div className="flex flex-row-reverse justify-between py-[30px] mx-[130px]">
       <div>
         <h2>Tööõigus</h2>
         <p>Tööõigus on valdkond, mis määrab kindlaks töötajate ja tööandjate õigused ning kohustused töösuhtes. Nõustame ja esindame tööandjaid ja eraisikutest kliente töövaidluskomisjonis ja kohtus. Aitame koostada töölepinguid ja vaatame üle olemasolevad töölepingud ning töösuhtes vajalikud dokumendid ja anname nõu nende täiendamiseks.</p>
       </div>
       <img src={toooigus} alt="" />
     </div>
-    <div>
+    <div className="flex justify-between pt-[100px] mx-[130px] pb-[50px]">
       <div>
         <h2>Võlaõigus</h2>
         <p>Võlaõiguse esemeks on lepinguõigus ja lepinguvälised kohustused. Lepinguõiguses reguleeritakse lepingutel põhinevaid õigussuhteid osapoolte vahel. Lepinguvälised võlasuhted on võlasuhted, mis tekivad muul viisil, kui lepingust tulenevalt. Lepinguvälisteks kohustusteks on tasu avalik lubamine, käsundita asjaajamine, alusetu rikastumine, kahju õigusvastane tekitamine. Nõustame ja esindame lepingute täitmisel ning teistes võlasuhtes tekkivate probleemide lahendamisel. </p>
       </div>
       <img src={volaoigus} alt="" />
     </div>
-    <div>
+    <div className="flex flex-row-reverse justify-between py-[30px] mx-[130px]">
       <div>
         <h2>Äriõigus</h2>
         <p>Osaühing, aktsiaselt, täisühing, usaldusühing, tulundusühistu – on eraõigluslik isikute ühendus, mis luuakse mingi eesmärgi saavutamiseks. Nõustame ja aitame lahendada äriühingu igapäevase majandustegevuse raames tekkivate küsimuste lahendamisel. Esindame äriühingute asutamise, seadusest tulenevate muudatuste tegemisel äriühingu dokumentatsioonis ja muudatuste kandmisel äriregistrisse.</p>
       </div>
       <img src={arioigus} alt="" />
     </div>
-    <div>
+    <div className="flex justify-between pt-[100px] mx-[130px] pb-[50px]">
       <div>
         <h2>Vaidluste lahendamine. Haldus- ja tsiviilkohtumenetlused</h2>
         <p>Vaidluste lahendamine erinevate valdkondade haldus- ja tsiviilasjades moodustab olulise osa meie advokaadibüroo tegevustest. Enam kui 25 aastat osalemist kohtuvaidlustes haldus- ja eraõiguse valdkonna erinevates asjades on meile andnud hindamatu kogemuse parima lahenduse leidmiseks kliendi huvide kaitsel. Samas, saame oma kogemuste pinnalt väita, et vaidlevate poolte huvid saavad kõige optimaalsemal viisil tagatud läbirääkimiste tulemusena ja kohtumenetlust ei olegi vaja.</p>
