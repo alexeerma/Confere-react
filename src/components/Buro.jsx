2bdb8296f39c0aa1c88e9ceaa9fa281bb6becfca
alexeerma <aleksander.eerma@hotmail.com>
2023-03-29 16:31:18 +0300

    Hero Responsive

--- src/components/Buro.jsx
+++ src/components/Buro.jsx
@@ -7,7 +7,7 @@ const Buro = () => (
   <section className="bg-conferehele">
     <div>
       <h1 className='text-center text-primary font-displayfair font-semibold text-6xl pt-[100px] pb-[60px]'>Büroo</h1>
-      <div className='flex text-justify justify-between gap-5 py-[60px] mx-[120px] sm:flex'>
+      <div className='w-screen sm:flex text-justify justify-between gap-5 py-[60px] mx-[120px]'>
         <div className='w-[550px]'>
           <p className="font-nunito">Confere Advokaadibüroo OÜ asutajaks on vandeadvokaat Kersti Kägi, kes advokaadina on tegutsenud üle 25 aasta. Meie tugevuseks on pikaajaline õigusnõustamise ja kohtumenetlustes esindamise kogemus enamikes õigusvaldkondades. Meie hindame professionaalsust, põhjalikkust, kvaliteeti, ratsionaalsust ja juhindume oma töös nendest väärtustest. Vajadusel teeme koostööd erinevate teiste elualade spetsialistidega ja kolleegidega teistest advokaadibüroodest.</p>
         </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import { Button } from "../components";
 
 const Hero = () => ( 
   <section className="relative w-full h-screen">
-    <div className="flex content-center pt-[350px]">
+    <div className="sm:flex content-center pt-[350px] md:pr-[25px]">
       <div className="basis-1/2"></div>
       <div className="shrink basis-1/2 text-center flex-col justify-items-center items-center">
         <img src={logo} alt="Confere" className="h-[60px] mx-auto" />
