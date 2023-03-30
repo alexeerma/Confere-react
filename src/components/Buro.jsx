0b478c0363e101ddc127dbbde9adcd5b6094b832
alexeerma <aleksander.eerma@hotmail.com>
2023-03-30 16:28:22 +0300

    Goals section responsiveness

--- src/components/Buro.jsx
+++ src/components/Buro.jsx
@@ -6,12 +6,12 @@ const Buro = () => (
   
   <section className="bg-conferehele">
     <div>
-      <h1 className='text-center text-primary font-displayfair font-semibold text-6xl pt-[100px] pb-[60px]'>Büroo</h1>
-      <div className='md:justify-center items-center flex flex-col-reverse md:flex-row  px-5 py-20 md:py-10 gap-10 w-screen mx-auto'>
-        <div className='w-[550px]'>
-          <p className="font-nunito mx-[55px]">Confere Advokaadibüroo OÜ asutajaks on vandeadvokaat Kersti Kägi, kes advokaadina on tegutsenud üle 25 aasta. Meie tugevuseks on pikaajaline õigusnõustamise ja kohtumenetlustes esindamise kogemus enamikes õigusvaldkondades. Meie hindame professionaalsust, põhjalikkust, kvaliteeti, ratsionaalsust ja juhindume oma töös nendest väärtustest. Vajadusel teeme koostööd erinevate teiste elualade spetsialistidega ja kolleegidega teistest advokaadibüroodest.</p>
+      <h1 className='text-center text-primary font-displayfair font-semibold text-6xl pt-[100px] pb-[30px]'>Büroo</h1>
+      <div className='md:flex flex-col-reverse md:flex-row px-10 py-20 md:py-10 justify-around w-screen mx-auto'>
+        <div className='w-auto'>
+          <p className="font-nunito mx-auto text-justify pb-5">Confere Advokaadibüroo OÜ asutajaks on vandeadvokaat Kersti Kägi, kes advokaadina on tegutsenud üle 25 aasta. Meie tugevuseks on pikaajaline õigusnõustamise ja kohtumenetlustes esindamise kogemus enamikes õigusvaldkondades. Meie hindame professionaalsust, põhjalikkust, kvaliteeti, ratsionaalsust ja juhindume oma töös nendest väärtustest. Vajadusel teeme koostööd erinevate teiste elualade spetsialistidega ja kolleegidega teistest advokaadibüroodest.</p>
         </div>
-        <div className='mx-[10px]'>
+        <div className=''>
           <img src={buroo} alt="Büroo" />
         </div>
       </div>
--- src/components/Goals.jsx
+++ src/components/Goals.jsx
@@ -6,10 +6,10 @@ const Goals = () => (
   
   <section className="bg-secondary">
     <div>
-      <h1 className='text-center text-primary font-displayfair font-semibold text-6xl pt-[100px] pb-[60px]'>Väärtused</h1>
-      <div className='flex text-justify justify-between py-[60px] mx-[120px]'>
-        <div className='w-[550px]'>
-          <p className="font-nunito">Confere Advokaadibüroo alustas tegevust 2023. aasta jaanuaris. Confere Advokaadibüroo asutajaks on vandeadvokaat Kersti Kägi, kellel kogemust advokaadi ja vandeadvokaadina on üle 25 aasta. Meie tugevuseks on pikaajaline õigusnõustamise ja kohtumenetlustes esindamise kogemus. Meie prioriteediks kliendile õigusteenuse osutamisel on professionaalsus, põhjalikkus, kvaliteet. Teeme vajadusel koostööd ka teiste elualade spetsialistidega ja kolleegidega teistest advokaadibüroodest, mis võimaldab meil saavutada parim võimalik tulemus Teie küsimuse või probleemi lahendamisel. Eesmärgi saavutamiseks täiendame ka pidevalt oma teadmisi ja oskusi.Me nõustame Teid eesti, inglise, vene keeles.Kui vajate nõu ja abi eelnimetatud valdkondades, saatke palun oma küsimus meie büroo üldmeilile info@conferelaw.ee ja vastame Teile esimesel võimalusel.</p>
+      <h1 className='text-center text-primary font-displayfair font-semibold text-6xl pt-[100px] pb-[30px]'>Väärtused</h1>
+      <div className='md:flex flex-col-reverse md:flex-row px-10 py-20 md:py-10 justify-around w-screen mx-auto'>
+        <div className='w-auto'>
+          <p className="font-nunito mx-auto text-justify pb-5">Confere Advokaadibüroo alustas tegevust 2023. aasta jaanuaris. Confere Advokaadibüroo asutajaks on vandeadvokaat Kersti Kägi, kellel kogemust advokaadi ja vandeadvokaadina on üle 25 aasta. Meie tugevuseks on pikaajaline õigusnõustamise ja kohtumenetlustes esindamise kogemus. Meie prioriteediks kliendile õigusteenuse osutamisel on professionaalsus, põhjalikkus, kvaliteet. Teeme vajadusel koostööd ka teiste elualade spetsialistidega ja kolleegidega teistest advokaadibüroodest, mis võimaldab meil saavutada parim võimalik tulemus Teie küsimuse või probleemi lahendamisel. Eesmärgi saavutamiseks täiendame ka pidevalt oma teadmisi ja oskusi.Me nõustame Teid eesti, inglise, vene keeles.Kui vajate nõu ja abi eelnimetatud valdkondades, saatke palun oma küsimus meie büroo üldmeilile info@conferelaw.ee ja vastame Teile esimesel võimalusel.</p>
         </div>
         <div>
           <img src={vaartused} alt="Väärtused" />
