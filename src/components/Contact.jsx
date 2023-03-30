897c738a341a08d43834e1c19fd5794e1f2ea2c3
alexeerma <aleksander.eerma@hotmail.com>
2023-03-30 16:40:30 +0300

    Contact component responsive

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -63,22 +63,22 @@ const Contact = () => {
   };
 
     return (
-      <section className="bg-conferehele px-[150px] pb-[100px]">
+      <section className="bg-conferehele px-[50px] pb-[100px]">
         <div className='mb-6'>
           <h1 className="text-center pt-[80px] pb-[50px] font-displayfair text-primary font-bold text-5xl">Kontakt</h1>
         </div>
-        <div className="flex justify-around">
+        <div className="md:flex justify-around justify-center ">
           <form
           ref={formRef}
           onSubmit={handleSubmit}
-          className='mt-9 flex flex-col gap-8'>
+          className='mt-9 flex flex-col gap-8 items-center justify-center'>
             <input
               type='text'
               name='name'
               value={form.name}
               onChange={handleChange}
               placeholder="Ees- ja perekonnanimi"
-              className='bg-primary py-4 px-6 placeholder:text-black text-black font-nunito rounded-lg outline-none border-none font-medium'
+              className='bg-primary py-4 px-6 placeholder:text-black text-black font-nunito rounded-lg outline-none border-none font-medium min-w-[300px]'
             />
             <input
               type='email'
@@ -86,7 +86,7 @@ const Contact = () => {
               value={form.email}
               onChange={handleChange}
               placeholder="E-mail"
-              className='bg-primary py-4 px-6 placeholder:text-black text-black font-nunito rounded-lg outline-none border-none font-medium'
+              className='bg-primary py-4 px-6 placeholder:text-black text-black font-nunito rounded-lg outline-none border-none font-medium min-w-[300px]'
             />
             <textarea
               rows={7}
@@ -94,18 +94,18 @@ const Contact = () => {
               value={form.message}
               onChange={handleChange}
               placeholder='Teade'
-              className='bg-primary py-4 px-6 placeholder:text-black text-black font-nunito rounded-lg outline-none border-none font-medium'
+              className='bg-primary py-4 px-6 placeholder:text-black text-black font-nunito rounded-lg outline-none border-none font-medium min-w-[300px]'
             />
             <button
             type='submit'
-            className='bg-primary py-3 px-8 rounded-xl outline-none w-fit text-black font-nunito shadow-md shadow-primary'
+            className='bg-primary hover:bg-secondary py-3 px-8 rounded-xl outline-none w-fit text-black font-nunito shadow-md shadow-primary min-w-[100px]'
           >
             {loading ? "Sending..." : "Send"}
           </button>
           </form>
-          <div className='mt-9'>
-            <img src={logo_black} alt="" className="max-w-[300px] "/>
-            <h4 className="text-primary text-3xl font-nunito pb-[80px]">Advokaadibüroo</h4>
+          <div className='mt-9 flex-col justify-center items-center pt-10'>
+            <img src={logo_black} alt="" className="md:max-w-[300px] "/>
+            <h4 className="text-primary uppercase text-center text-3xl font-nunito pb-[30px]">Advokaadibüroo</h4>
             <p className='text-center'>Aadress: Tartu mnt 83, Tallinn, 10115, Eesti</p>
             <p className='text-center'>Telefon: +372 6740433</p>
             <p className='text-center'>Email: info@conferelaw.ee</p>
